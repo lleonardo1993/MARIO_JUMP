2f20a4de5ed071f40586ab6d1dea0b78bacc89e0
Leonardo <lleonardo1555@gmail.com>
2023-02-19 11:33:59 -0300

    lóica das animacões

---
+++ src/js/script.js
@@ -0,0 +1,41 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+
+/** logica do jump*/
+const jump = () => {
+    mario.classList.add('jump');
+
+
+    setTimeout(() => {
+        mario.classList.remove('jump');
+
+    }, 500);
+}
+
+document.addEventListener('keydown', jump);
+
+/** logica do tubo game over*/
+
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft; /** pega o valor de toda posicao*/
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', ''); /** pega o valor do style, e convert para string com sinal ' + '*/
+
+    console.log(marioPosition)
+    if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 90){
+
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = './src/assets/game-over.png';
+        mario.style.width = '75px';
+        mario.style.marginLeft = '50px';
+        
+
+
+    }
+
+}, 10)
