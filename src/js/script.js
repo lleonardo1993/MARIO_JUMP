55f4f33e47bef5f690c68c663010c304e077275b
Leonardo <lleonardo1555@gmail.com>
2023-02-20 15:36:01 -0300

    condições setTimeout, finish

--- src/js/script.js
+++ src/js/script.js
@@ -6,6 +6,7 @@ const overlay = document.querySelector('.overlay');
 const reset = document.querySelector('.reset');
 const overlayScore = document.querySelector('.overlay-score');
 const startGameInfo = document.querySelector('.start-game');
+const win = document.querySelector('.win');
 
 let countScore = 0;
 let startGame = true;
@@ -14,9 +15,9 @@ let timerScore;
 let timerSpeed;
 
 startGameInfo.innerHTML =
-  'Pressione qualquer tecla para iniciar <br/> O tempo é contabilizado a cada segundo';
+    'Pressione qualquer tecla para iniciar <br/> O tempo é contabilizado a cada segundo';
 
-  /** restart game*/
+/** restart game*/
 reset.addEventListener('click', () => window.location.reload());
 
 window.addEventListener('keypress', () => {
@@ -28,31 +29,81 @@ window.addEventListener('keypress', () => {
     setTimeout(() => mario.classList.remove('jump'), 500);
 
     if (startGame) {
-      let pipeSpeed = 1.5;
-      startGameInfo.innerHTML = '';
-      startGameInfo.style.background = 'transparent';
-      timerScore = setInterval(() => {
-        countScore++;
-        score.innerHTML = `SCORE ${countScore}`;
-      }, 1000);
-  
-      timerSpeed = setInterval(() => {
-        pipeSpeed -= 0.1;
-        if (pipeSpeed <= 0) {
-          pipeSpeed = 0.6;
-        }
-        console.log({ pipeSpeed });
-        pipe.style.animationDelay = `pipe-animate ${pipeSpeed}s infinite linear`;
-      }, 1000 * 10);
+        let pipeSpeed = 1.5;
+        startGameInfo.innerHTML = '';
+        startGameInfo.style.background = 'transparent';
+        timerScore = setInterval(() => {
+            countScore++;
+            score.innerHTML = `SCORE ${countScore}`;
+        }, 1000);
+
+        timerSpeed = setInterval(() => {
+            pipeSpeed -= 0.1;
+            if (pipeSpeed <= 0) {
+                pipeSpeed = 0.6;
+            }
+            console.log({ pipeSpeed });
+            pipe.style.animationDelay = `pipe-animate ${pipeSpeed}s infinite linear`;
+        }, 1000 * 10);
     }
+
+    if (countScore > 2) {
+        winner();
+        clearInterval(winner)
+        winnerTwo();
+        winnerTree();
+        
+    }
+
 
     startGame = false;
 
     timerVerifyDead = setInterval(() => {
-      handleLogicForGameOver();
+        loop();
     }, 10);
 });
 
+const winnerTwo = () => {
+    setTimeout(() => {
+        mario.style.width = '100%';
+        mario.style.width = '100%';
+        mario.src = '/src/assets/princ.gif';
+
+    }, 13000);
+}
+const winnerTree = () => {
+    setTimeout(() => {
+        mario.style.width = '100%';
+        mario.style.width = '100%';
+        mario.src = '/src/assets/win2.gif';
+
+    }, 18000);
+}
+
+const winner = () => {
+
+    const pipePosition = pipe.offsetLeft; /** pega o valor de toda posicao*/
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', ''); /** pega o valor do style, e convert para string com sinal ' + '*/
+
+    pipe.style.animation = 'none';
+    pipe.style.left = `${pipePosition}px`;
+
+    mario.style.animation = 'none';
+    mario.style.bottom = `${marioPosition}px`;
+    pipe.style.width = '0%';
+
+    setTimeout(() => {
+        mario.src = '/src/assets/win1.gif';
+        mario.style.width = '100%';
+        mario.style.width = '100%';
+
+    });
+
+
+    clearInterval(loop);
+    clearInterval(timerScore);
+    clearInterval(timerVerifyDead);
+}
 /** start game*/
 
 
@@ -73,13 +124,13 @@ document.addEventListener('keydown', jump);
 
 /** logica do tubo game over*/
 
-const loop = setInterval(() => {
+const loop = () => {
 
     const pipePosition = pipe.offsetLeft; /** pega o valor de toda posicao*/
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', ''); /** pega o valor do style, e convert para string com sinal ' + '*/
 
     console.log(marioPosition)
-    if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 90){
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 90) {
 
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
@@ -96,10 +147,10 @@ const loop = setInterval(() => {
         overlayScore.innerHTML = `SCORE ${countScore}`;
         overlay.style.display = 'flex';
         /** stop loop*/
-       
-        clearInterval(loop); 
+
+        clearInterval(loop);
         clearInterval(timerScore);
         clearInterval(timerVerifyDead);
     }
 
-}, 10)
+}
